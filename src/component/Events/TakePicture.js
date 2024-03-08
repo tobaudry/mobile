7a90223baf07905100eda9906d953621b3d24834
Baudry <tobaudry@ensc.fr>
2024-03-08 11:34:53 +0100

    maj cam

--- src/component/Events/TakePicture.js
+++ src/component/Events/TakePicture.js
@@ -1,48 +1,27 @@
-// Importez useState en haut de votre fichier
-import React, { useRef, useEffect, useState } from "react";
+import React, { useRef, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import { storage } from "../../firebase-config";
 import { ref, uploadBytes } from "firebase/storage";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import FlashOnIcon from "@mui/icons-material/FlashOn";
 import FlashOffIcon from "@mui/icons-material/FlashOff";
 import LoopIcon from "@mui/icons-material/Loop";
+import CameraAltIcon from "@mui/icons-material/CameraAlt";
 
 import "./TakePicture.css";
 
 export default function TakePicture() {
   const videoRef = useRef(null);
+  const navigate = useNavigate();
   const { eventId } = useParams();
   const [cameraActive, setCameraActive] = useState(true);
   const [flashOn, setFlashOn] = useState(false);
-  const [cameraFacingMode, setCameraFacingMode] = useState("environment"); // "user" for front camera, "environment" for back camera
+  const [cameraFacingMode, setCameraFacingMode] = useState("environment");
 
-  useEffect(() => {
-    const videoRefCurrent = videoRef.current;
-  
-    const handleCapture = async () => {
-      try {
-        const stream = await navigator.mediaDevices.getUserMedia({
-          video: {
-            facingMode: cameraFacingMode // Utiliser la caméra avant ou arrière selon l'état de cameraFacingMode
-          }
-        });
-        videoRefCurrent.srcObject = stream;
-      } catch (error) {
-        console.error("Erreur lors de l'accès à la caméra:", error);
-      }
-    };
-  
-    handleCapture();
-  
-    return () => {
-      if (videoRefCurrent && videoRefCurrent.srcObject) {
-        const tracks = videoRefCurrent.srcObject.getTracks();
-        tracks.forEach((track) => track.stop());
-      }
-    };
-  }, [cameraFacingMode]); // Assurez-vous de réagir aux changements de cameraFacingMode
+  const handleEventClickNavigate = () => {
+    navigate(`/event/${eventId}`);
+  };
 
   const handleSnapshot = async () => {
     setCameraActive(false);
@@ -56,39 +35,17 @@ export default function TakePicture() {
     canvas.height = videoRef.current.videoHeight;
     const ctx = canvas.getContext("2d");
 
-    // Logique du flash
-    if (flashOn) {
-      // Activer le flash
-      const popup = document.createElement("div");
-      popup.style.position = "fixed";
-      popup.style.top = 0;
-      popup.style.left = 0;
-      popup.style.width = "100%";
-      popup.style.height = "100%";
-      popup.style.background = "white";
-      popup.style.zIndex = 9999;
-      document.body.appendChild(popup);
+    // Draw the current frame from the video element onto the canvas
+    ctx.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height);
 
-      // Déclencher la prise de photo après la moitié du temps du flash
-      setTimeout(() => {
-        document.body.removeChild(popup);
-        takePhoto();
-      }, 500); // 250 ms pour prendre la photo au milieu du flash
-    } else {
-      // Si le flash est désactivé, prendre la photo directement
-      takePhoto();
-    }
+    // Convert the canvas content to a data URL representing the image
+    const dataUrl = canvas.toDataURL('image/jpeg');
 
-    // Fonction pour prendre la photo
-    function takePhoto() {
-      ctx.translate(canvas.width, 0);
-      ctx.scale(-1, 1);
-      ctx.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height);
-      canvas.toBlob(async (blob) => {
-        await uploadBytes(imgRef, blob);
-        setCameraActive(true); // Réactivez la caméra après la capture de la photo
-      }, "image/jpeg");
-    }
+    // Upload the image data to Firebase Storage
+    await uploadBytes(imgRef, dataUrl);
+
+    // Reset camera active state
+    setCameraActive(true);
   };
 
   const toggleFlash = () => {
@@ -96,7 +53,6 @@ export default function TakePicture() {
   };
 
   const toggleCameraFacingMode = () => {
-    // Basculez entre la caméra avant et arrière
     setCameraFacingMode(cameraFacingMode === "environment" ? "user" : "environment");
   };
 
@@ -104,7 +60,7 @@ export default function TakePicture() {
     <div>
       <div className="HeadEvent">
         <div className="icon">
-          <ArrowBackIosIcon onClick={() => setCameraActive(true)} />
+          <ArrowBackIosIcon onClick={handleEventClickNavigate} />
           <div>
             {flashOn ? (
               <FlashOnIcon onClick={toggleFlash} />
@@ -119,15 +75,18 @@ export default function TakePicture() {
 
       {cameraActive && (
         <button className="AddEventButton" onClick={handleSnapshot}>
-          +
+          <CameraAltIcon />
         </button>
       )}
+
       <div className="Camera">
         <video
           ref={videoRef}
           autoPlay
-          style={{ width: "100%", transform: "scaleX(-1)" }}
-        ></video>
+          playsInline
+          muted
+          style={{ transform: `scaleX(${cameraFacingMode === 'user' ? -1 : 1})` }}
+        />
       </div>
     </div>
   );
