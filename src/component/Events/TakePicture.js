f9ff54d78bb5823542d65c32e5aded3da392060e
Baudry <tobaudry@ensc.fr>
2024-03-12 11:27:50 +0100

    maj cam

--- src/component/Events/TakePicture.js
+++ src/component/Events/TakePicture.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { v4 as uuidv4 } from "uuid";
 import { storage } from "../../firebase-config";
 import { ref, uploadBytes } from "firebase/storage";
@@ -8,43 +8,17 @@ import FlashOnIcon from "@mui/icons-material/FlashOn";
 import FlashOffIcon from "@mui/icons-material/FlashOff";
 import LoopIcon from "@mui/icons-material/Loop";
 import CameraAltIcon from "@mui/icons-material/CameraAlt";
+import Webcam from 'react-webcam';
 
 import "./TakePicture.css";
 
 export default function TakePicture() {
-  const videoRef = useRef(null);
   const navigate = useNavigate();
   const { eventId } = useParams();
   const [cameraActive, setCameraActive] = useState(true);
-  const [flashOn, setFlashOn] = useState(false);
-  const [cameraFacingMode, setCameraFacingMode] = useState("environment");
-
-  useEffect(() => {
-    const constraints = { video: { facingMode: cameraFacingMode } };
-  
-    navigator.mediaDevices
-      .getUserMedia(constraints)
-      .then((stream) => {
-        if (videoRef.current) {
-          videoRef.current.srcObject = stream;
-        }
-      })
-      .catch((error) => {
-        console.error("Error accessing media devices: ", error);
-      });
-  
-    return () => {
-      // Cleanup: Stop the video stream when component unmounts
-      if (videoRef.current && videoRef.current.srcObject) {
-        const stream = videoRef.current.srcObject;
-        const tracks = stream.getTracks();
-  
-        tracks.forEach((track) => {
-          track.stop();
-        });
-      }
-    };
-  }, [cameraFacingMode]);
+  const [flash, setFlash] = useState(false);
+  const [facingMode, setFacingMode] = useState('environment');
+  const webcamRef = useRef(null); 
 
   const handleEventClickNavigate = () => {
     navigate(`/event/${eventId}`);
@@ -57,28 +31,18 @@ export default function TakePicture() {
     const folderPath = `events/${eventId}/${uid}`;
     const imgRef = ref(storage, folderPath);
 
-    const canvas = document.createElement("canvas");
-    canvas.width = videoRef.current.videoWidth;
-    canvas.height = videoRef.current.videoHeight;
-    const ctx = canvas.getContext("2d");
-
-    // Draw the current frame from the video element onto the canvas
-    ctx.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height);
-
-    // Convert the canvas content to a data URL representing the image
-    const dataUrl = canvas.toDataURL("image/jpeg");
+    const imageSrc = webcamRef.current.getScreenshot();
 
     // Upload the image data to Firebase Storage
-    await uploadBytes(imgRef, dataUrl);
+    await uploadBytes(imgRef, imageSrc);
 
     // Reset camera active state
     setCameraActive(true);
   };
 
-  const toggleFlash = () => {
-    setFlashOn(!flashOn);
+  const handleToggleFlash = () => {
+    setFlash(prevFlash => !prevFlash);
   };
-
 
   return (
     <div>
@@ -86,10 +50,10 @@ export default function TakePicture() {
         <div className="icon">
           <ArrowBackIosIcon onClick={handleEventClickNavigate} />
           <div>
-            {flashOn ? (
-              <FlashOnIcon onClick={toggleFlash} />
+            {flash ? (
+              <FlashOnIcon onClick={handleToggleFlash} />
             ) : (
-              <FlashOffIcon onClick={toggleFlash} />
+              <FlashOffIcon onClick={handleToggleFlash} />
             )}
             <LoopIcon/>
           </div>
@@ -104,12 +68,12 @@ export default function TakePicture() {
       )}
 
       <div className="Camera">
-        <video
-          ref={videoRef}
-          autoPlay
-          playsInline
-          muted
-          style={{ transform: `scaleX(${cameraFacingMode === 'user' ? -1 : 1})` }}
+        <Webcam
+          audio={false}
+          screenshotFormat="image/jpeg"
+          videoConstraints={{ facingMode: facingMode }}
+          flash={flash}
+          ref={webcamRef}
         />
       </div>
     </div>
